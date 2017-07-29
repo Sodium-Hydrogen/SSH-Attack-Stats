215e6c593ee44aeb210ac8486a653aa8300d5d84
Michael Julander <1110mdj@gmail.com>
2017-07-29 10:54:06 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+read -rp "To install you will need sudo level permissions. Continue? [y/n] " key
+
+if [ $key = "y" ] || [ $key = "Y" ]; then
+	cp ./42-log-stats /etc/update-motd.d/
+	chmod 755 /etc/update-motd.d/42-log-stats
+	g++ ./stats.cpp -o /usr/local/bin/log-stats.bin
+	echo "done"
+fi
