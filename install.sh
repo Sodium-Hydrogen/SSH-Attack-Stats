1c38c2314f24d3b0374ac613e44a627d57c77982
Michael Julander <1110mdj@gmail.com>
2017-08-15 14:12:10 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ read -rp "To install you will need sudo level permissions. Continue? [y/n] " key
 if [ $key = "y" ] || [ $key = "Y" ]; then
 	cp ./42-log-stats /etc/update-motd.d/
 	chmod 755 /etc/update-motd.d/42-log-stats
-	g++ ./stats.cpp -o /usr/local/bin/log-stats.bin
+	g++ ./stats.cpp -o /usr/local/bin/log-stats
 	cp ./conversion.txt /usr/local/bin/conversion.txt
 	echo "done"
 fi
